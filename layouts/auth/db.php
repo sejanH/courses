9c4848771c35c090c8adb0b8d29c39b46a3449d4
sejanH <sejan840@gmail.com>
2017-02-20 18:58:04 +0600

    db

--- layouts/auth/db.php
+++ layouts/auth/db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "mysql7.000webhost.com";
-$username = "a4163525_sejan";
-$password = "faucet30";
-$db = "a4163525_faucet";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "test";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$db);
 
