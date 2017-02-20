f6185048fc8fa17329ac52a650b966e0e47acd1f
sejanH <sejan840@gmail.com>
2017-02-20 19:23:59 +0600

    db

--- layouts/auth/db.php
+++ layouts/auth/db.php
@@ -13,45 +13,29 @@ if (mysqli_connect_errno())
   }
 
  $routine ="CREATE TABLE IF NOT EXISTS `routine` (
-
   `std_id` varchar(20) NOT NULL,
-
   `semester` varchar(15) NOT NULL,
-
   `course_code` varchar(20) NOT NULL,
-
   `section` int(5) NOT NULL,
-
   `starts` time NOT NULL,
-
   `ends` time NOT NULL,
-
   `weekdays` varchar(5) NOT NULL,
-
   `room` int(11) NOT NULL,
-
   KEY `semester` (`semester`)
-
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
 mysqli_query($conn,$routine) or die("ERROR: ". mysqli_connect_error());
 
-$std_dtls = "CREATE TABLE IF NOT EXISTS `student_info` (
-
+//creating table for the first time
+  $std_dtls = "CREATE TABLE IF NOT EXISTS `student_info` (
   `id` varchar(20) NOT NULL,
-
   `std_name` varchar(100) NOT NULL,
-
   `email` varchar(150) DEFAULT NULL,
-
   `DoB` date DEFAULT NULL,
-
   `pass` varchar(20) NOT NULL,
-
   `starting_semester` varchar(25) DEFAULT NULL,
-
   PRIMARY KEY (`id`),
-
   UNIQUE KEY `email` (`email`)
-
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
 mysqli_query($conn,$std_dtls) or die("ERROR: ". mysqli_connect_error());
+
+
